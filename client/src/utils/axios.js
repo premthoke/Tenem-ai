c544d9b4a9e725cb52715b13e8651ec16a0f5be1
premthoke <premthoke02@gmail.com>
2026-02-25 04:03:29 +0530

    Fix axios file

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,12 +4,4 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default instance;
+export default axiosInstance;
